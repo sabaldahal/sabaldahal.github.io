64adc834cc63a9692fc7dfa484022a3b32146ce8
dahalsl <sabal.dahal00@gmail.com>
2022-11-13 14:32:40 -0500

    Update main.js

--- apps/lg sim/main.js
+++ apps/lg sim/main.js
@@ -3,25 +3,27 @@ import { Input } from './lgsinput.js';
 import { Wire } from './wire.js';
 import { InputBit } from './inputbit.js';
 import { OutputBit } from './outputbit.js';
+import { Bottom } from './bottompanel.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth -6;
-    canvas.height = window.innerHeight - 200;
+    canvas.height = window.innerHeight - 6;
     class Board {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.bottom = new Bottom(this);
             this.inputBit = new InputBit(this);
             this.outputBit = new OutputBit(this);
             this.allGate = [];
             this.gate1 = new Gate(this);
-            this.gate1.width = 200;
+            //this.gate1.width = 200;
             this.gate1.update();
             this.gate2 = new Gate(this);
-            this.gate1.text = 'Coming';
-            this.gate2.text = 'Soon';
+            this.gate1.text = 'AND';
+            this.gate2.text = 'NOT';
             this.gate2.color = 'blue';
             this.gate2.x += 450;
             this.gate2.y += 200;
@@ -39,25 +41,29 @@ window.addEventListener('DOMContentLoaded', () => {
             context.clearRect(0, 0, this.width, this.height);
         }
 
-        addGate() {
+        addGate(one) {
             let gate = new Gate(this);
-            gate.x = 0;
-            gate.y = 0;
+            gate.x = this.width/2;
+            gate.y = this.height/2;
+            gate.input = one.input;
+            gate.output = one.output;
+            gate.color = one.color;
+            gate.text = one.name;
             gate.update();
-            gate.color = 'yellow';
             this.allGate.push(gate);
             this.draw(ctx)
         }
         
         draw(context) {
             this.clear(context);
-            this.wires.draw(context);
+            this.wires.calculate_signal();
             this.inputBit.draw(context);
             this.outputBit.draw(context);
             for (let one of this.allGate) {
                 one.draw(context);
             }
-
+            this.wires.draw(context);
+            this.bottom.draw(context);
         }
     }
 
@@ -66,9 +72,9 @@ window.addEventListener('DOMContentLoaded', () => {
     game.draw(ctx);
 
 
-    //inputs outside of canvas
-    const add_gate = document.querySelector('#try');
-    add_gate.onclick = () => {
-        game.addGate();
-    }
+    // //inputs outside of canvas
+    // const add_gate = document.querySelector('#try');
+    // add_gate.onclick = () => {
+    //     game.addGate();
+    // }
 })
