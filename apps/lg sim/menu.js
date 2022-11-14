e486af19444f91525bc372567bc5c275f028d37b
dahalsl <sabal.dahal00@gmail.com>
2022-11-13 19:14:29 -0500

    Create menu.js

---
+++ apps/lg sim/menu.js
@@ -0,0 +1,49 @@
+
+const menuholder = document.querySelector('#canvas-popup');
+const openGateMenu = (x, y)=>{
+    menuholder.style.left = 100 + x + 'px';
+    menuholder.style.top = -50 + y + 'px';
+}
+
+export class Menu{
+    constructor(board){
+        this.board = board;
+        this.activeGate = null;
+        this.activeBit = null;
+        this.activeWire = null;
+        this.cntx = canvas.getContext('2d');
+
+        //interactive methods
+        menuholder.onclick = (e)=>{
+            alert('This Functionality will cause the program to crash; New update will fix this error');
+            let idx = this.board.allGate.findIndex((gate)=>gate.isIndex == this.activeGate);
+            this.board.allGate.splice(idx,1);
+            menuholder.style.display = 'none';
+            this.board.draw(this.cntx);
+        }
+    }
+
+    gate_menu(e){
+        //check for the button
+        if(e.button != 2) {
+            menuholder.style.display = 'none';
+            return;
+        }
+        //check for the gate index;
+        let val;
+        for(let gate of this.board.allGate){
+            val = gate.is_mouse_over(e.offsetX, e.offsetY);
+            if(val){
+                this.activeGate = gate.isIndex;
+                break;
+            }
+        }
+        if(!val) this.activeGate = null;
+        if(this.activeGate != null){
+            openGateMenu(e.pageX, e.pageY);
+            menuholder.style.display = 'block';
+        }else{
+            menuholder.style.display = 'none';
+        }
+    }
+}
