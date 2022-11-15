ebcb08cbbc006f5704bcec9734c43c717a41ca30
dahalsl <sabal.dahal00@gmail.com>
2022-11-14 20:29:47 -0500

    Create instructions.js

---
+++ apps/lg sim/instructions.js
@@ -0,0 +1,34 @@
+export const instructions = ()=>{
+    return `
+    <h4>Overview</h4>
+    <p>This application simulates the very fundamental working mechanism
+    of logic gates<br>
+    Begin with two gates: 'AND' and 'NOT' and work your way up to build
+    more complex structures
+    </p>
+
+    <h4>Adding Inputs and Outputs</h4>
+    <p>
+    You can add as many inputs and outputs as you like( if there's enough space left)<br>
+    Hover on the leftmost panel and click to add inputs <br>
+    Hover on the rightmost panel and click to add outputs
+    </p>
+
+    <h4>Adding Gates</h4>
+    <p>
+    Add Gates by clicking on the Gate buttons at the bottom of the page
+    </p>
+
+    <h4>Adding Wires</h4>
+    <p>
+    Click on any pin and move your mouse to the other pin. Click to add wire
+    </p>
+
+    <h4>Toggle bit 0 and 1</h4>
+    <p>
+    Click on the input to toggle the bits<br>
+    Red Color indicates 1<br>
+    Grey Color indicates 0
+    </p>
+    `
+}
